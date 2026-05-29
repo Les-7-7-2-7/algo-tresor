057bda90a9d5130dcab3f3674ff28749bcdc4e05
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2026-05-29 23:57:30 +0200

    accept incoming

--- src/main/java/dev/bastienluben/algotresor/Main.java
+++ src/main/java/dev/bastienluben/algotresor/Main.java
@@ -15,11 +15,7 @@ public class Main {
 		int sizeCapacity = Integer.parseInt(scanner.nextLine().split(" ")[1]);
 		int weightCapacity = Integer.parseInt(scanner.nextLine().split(" ")[1]);
 
-<<<<<<< HEAD
-		Game game = new Game(n, sizeCapacity, weightCapacity, new PassStrategy());
-=======
         Game game = new Game(n, sizeCapacity, weightCapacity, new GreedyStrategy());
->>>>>>> master
 
 		for (int i = 0; i < n; i++) {
 			game.addItem(Item.fromString(scanner.nextLine()));
@@ -28,18 +24,6 @@ public class Main {
 		scanner.nextLine(); // "preprocessing 5000"
 		game.preprocess();
 
-<<<<<<< HEAD
-		while (scanner.hasNextLine()) {
-			int takenId = Integer.parseInt(scanner.nextLine().split(" ")[1]); // "taken ID"
-			game.opponentTook(takenId);
-
-			scanner.nextLine(); // "next_item 500"
-			int choice = game.pickItem();
-			System.out.println(choice);
-			System.out.flush();
-		}
-	}
-=======
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             if (line.startsWith("taken")) {
@@ -52,5 +36,4 @@ public class Main {
             }
         }
     }
->>>>>>> master
 }
